227ffaf63a902ea4867d4c9c808afd27a93ed83c
«Rinat» <rtatarin949@gmail.com>
2022-07-21 20:34:58 +0300

    Task1_1

---
+++ 1_Lesson/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа на вход принимает число и выдает его квадрат
+Console.WriteLine("Write a number: "); 
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+Console.WriteLine(a*a);
+
+
+
