fae32cbc892aeccd63536b6c9775d8841a5e47c9
«Rinat» <rtatarin949@gmail.com>
2022-07-21 23:55:34 +0300

    Task1_3

---
+++ 1_Lesson/1_3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int a=int.Parse(Console.ReadLine());
+
+if(a==1)
+{
+  Console.WriteLine("ПН");  
+}
+else if(a==2)
+    {
+        Console.WriteLine("ВТ");
+    }
+else if(a==3)
+    {
+        Console.WriteLine("СР");
+    }
+else if(a==4)
+    {
+        Console.WriteLine("ЧТ");
+    }
+else if(a==5)
+    {
+        Console.WriteLine("ПТ");
+    }
+else if(a==6)
+    {
+        Console.WriteLine("СБ");
+    }
+else if(a==7)
+    {
+        Console.WriteLine("ВС");
+    }
+else
+    {
+        Console.WriteLine("Такого дня недели нет");
+    }
