4b3bfd2794db117650b872f27d1b67d72d82fb11
«Rinat» <rtatarin949@gmail.com>
2022-07-23 00:45:22 +0300

    task_4

---
+++ 1_Lesson/1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// 4. Напишите программу, которая на вход принимает одно число
+// (N), а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+// if (a<0)
+int i=-a;
+while (i<=a)
+{
+Console.WriteLine(i);
+i++;
+}
