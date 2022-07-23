33bcdd8dc203ba35c18360bb004191f1bf6f6a67
«Rinat» <rtatarin949@gmail.com>
2022-07-23 16:58:35 +0300

    HomeWork_Task7

---
+++ 1_Lesson/DZ_Task_7/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Write a number: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(n%10);
+
