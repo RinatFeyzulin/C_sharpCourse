29ec466f539dcd0d4f3ded22de565640186dc551
«Rinat» <rtatarin949@gmail.com>
2022-07-24 19:59:02 +0300

    HW_Task_2

---
+++ 2_Lesson/HW_Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int DeleteTwoNum(int Num) 
+{
+    int n1 = Num/100;
+    int n2=  Num%10;
+    Console.WriteLine($"{n1}{n2}");
+    return Num;
+}
+
+Console.WriteLine(DeleteTwoNum(new Random() .Next(100,1000)));
+
+
+
