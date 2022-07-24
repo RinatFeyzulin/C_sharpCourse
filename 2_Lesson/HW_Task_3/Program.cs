1db543d7a8011043d8bb1d3927d3f16a8e5755a7
«Rinat» <rtatarin949@gmail.com>
2022-07-24 21:01:15 +0300

    HW_Task_3

--- 2_Lesson/HW_Task_3/Program.cs
+++ 2_Lesson/HW_Task_3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int a=int.Parse(Console.ReadLine());
+
+int DeleteNum(int Num) 
+{
+    while(Num>=1000)
+    {
+        int result = Num/10;
+        Num=result;
+    }
+    int res=Num%10;
+    return res;
+}
+
+Console.WriteLine(DeleteNum(a));
