a0dcc69cb5cac541e463f6ff4db011736c3f3f57
«Rinat» <rtatarin949@gmail.com>
2022-07-25 19:16:14 +0300

    task_3_2

---
+++ 3_Lesson/3_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//2. Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+
+double Quarters(double x1, double y1, double x2, double y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2)),4);
+}
+
+double result = Quarters(3, 6, 2, 1);
+Console.WriteLine(result);
+
