a4a91043fa5ab5edfbf5f9bfd7a5e5322fc643c6
«Rinat» <rtatarin949@gmail.com>
2022-08-02 22:45:42 +0300

    Home Work Task1

---
+++ 4_Lesson/HW_Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int exponentiate(int a, int b)
+{
+    int result = a;
+    for(int i = 1;i<b; i++)
+    {
+        result = a*result;
+    }
+
+    return result;
+}
+
+Console.WriteLine(exponentiate(4,5));
