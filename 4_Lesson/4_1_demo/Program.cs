94a61231706a2e5eaac5c5ab632a92bffdda1041
«Rinat» <rtatarin949@gmail.com>
2022-08-02 19:48:17 +0300

    4_lesson Task4.1

---
+++ 4_Lesson/4_1_demo/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 1: Напишите программу, которая принимает
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int sum(int a)
+{
+    
+    int all_sum=0;
+    for(int i=1;i<=a;i++)
+    {
+        all_sum = all_sum + i;
+    }
+
+ return all_sum;
+}
+
+
+Console.WriteLine(sum(4));
