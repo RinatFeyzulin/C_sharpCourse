6dfd348432940f7b81c472ca0b1febe4c3f3e78e
«Rinat» <rtatarin949@gmail.com>
2022-08-02 20:02:47 +0300

    4_lesson Task4.11

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+void number(int a)
+{
+   string num = a.ToString();
+   Console.WriteLine(num.Length);
+}
+
+number(654345);
