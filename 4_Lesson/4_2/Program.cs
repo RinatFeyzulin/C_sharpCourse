773f8ad0453e48fa67fc7498f7804b069824eb1a
«Rinat» <rtatarin949@gmail.com>
2022-08-02 20:59:35 +0300

    4_lesson Task4.2

---
+++ 4_Lesson/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+int Number(int a)
+{
+    int result = 1;
+    for(int i = 1; i<=a; i++)
+    {
+       result=i*result;
+    }
+
+    return result;
+}
+
+Console.WriteLine(Number(5));
