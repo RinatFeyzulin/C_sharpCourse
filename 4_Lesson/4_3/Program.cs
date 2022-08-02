e485deddfdef14230ac9750b02e7bfca2dd5db6a
«Rinat» <rtatarin949@gmail.com>
2022-08-02 22:23:05 +0300

    4_lesson Task4.3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+
+
+
+void ArrRandom()
+{
+    Random random = new Random();
+    int [] myArray = new int[8];
+    for(int i = 0;i<myArray.Length;i++)
+    {
+        myArray[i]= random.Next(0,2);
+    }
+
+    for(int i = 0;i<myArray.Length;i++)
+    {
+        Console.Write(myArray[i]);
+    }
+}
+
+ArrRandom();
