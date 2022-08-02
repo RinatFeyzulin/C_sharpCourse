172bbfd33d8eb2d577021036e9dcca092b36300e
«Rinat» <rtatarin949@gmail.com>
2022-08-03 00:58:59 +0300

    Home Work Task3

---
+++ 4_Lesson/HW_Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void ArrRandom(ref int [] myArray)
+{
+    Random random = new Random();
+    for(int i = 0;i<myArray.Length;i++)
+    {
+        myArray[i]= random.Next(100);
+    }
+}
+
+void conclusion(ref int[] myArray)
+{
+    for(int i =0; i<myArray.Length;i++)
+    {
+        Console.Write($"{myArray[i]}  ");
+    }
+}
+
+int [] myArray = new int[8];
+ArrRandom(ref myArray);
+conclusion(ref myArray);
+
