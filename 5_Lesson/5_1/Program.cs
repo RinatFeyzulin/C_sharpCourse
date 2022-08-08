49471a9876151631381dd9117263c057562adaa3
«Rinat» <rtatarin949@gmail.com>
2022-08-09 00:05:15 +0300

    5.1Demo Task

---
+++ 5_Lesson/5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿ int [] arr = new int [12];
+Random random = new Random();
+void arrRandom(int []arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(-9,9);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+}
+
+void PozNeg(int []arr)
+{
+   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]=arr[i]*-1} ");
+    }
+    
+}
+
+arrRandom(arr);
+PozNeg(arr);
---
+++ 5_Lesson/5_1Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿// задача 1. Задайте массив из 12 элементов, заполненный случайными числами
+// из промежутка [-9,9]. Найдите сумму отрицательных и положительных элементов массива.
+
+
+int [] arr = new int [12];
+Random random = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(-9,9);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+
+void sumPozNeg(int []arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>=0)
+        pos+=arr[i];
+        else neg+=arr[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Possitiv ={pos}, Negative{neg}");
+}
+
+sumPozNeg(arr);
