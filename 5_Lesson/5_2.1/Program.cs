3272071b4c39c69f6d1bdfe89a95c64e1f0111b3
«Rinat» <rtatarin949@gmail.com>
2022-08-09 00:32:29 +0300

    5.2 Task

---
+++ 5_Lesson/5_2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2. Зайдайте массив. напишите программу,котора
+// определяет, присутствует ли заданное число в массиве.
+
+int [] myArray = {1,44,4,6,77,5,34,3,2,0,89,54,-3};
+void ArrayEnumeration(int [] arr)
+{
+    Console.WriteLine("Write a number");
+    int n = int.Parse(Console.ReadLine());
+    string flag = "no";
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(n == arr[i])
+        {
+            flag = "yes";
+            break;
+        }
+    }
+    Console.WriteLine(flag);
+}
+ArrayEnumeration(myArray);
