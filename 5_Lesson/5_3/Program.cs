8b2e8adfb7ab8e91a6be65fbe9e0cad5ce1465ad
«Rinat» <rtatarin949@gmail.com>
2022-08-09 00:54:26 +0300

    5.3 Task

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int [] arr = new int [123];
+Random random = new Random();
+void arrRandom(int []arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(15);
+}
+
+}
+
+    
+
+void Google(int []arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>9 && arr[i]<100) count+=1;
+    }
+    Console.Write(count);
+}
+
+arrRandom(arr);
+Google(arr);
+
