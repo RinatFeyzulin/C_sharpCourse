ebe7477dd61ab8b27eca44540c698f758f9830e9
«Rinat» <rtatarin949@gmail.com>
2022-08-12 20:12:10 +0300

    HomeTask1

---
+++ 6_Lesson/HomeTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Counter()
+{
+    int count =0;
+    for (int i = 0;; i++)
+    {
+        string num = Console.ReadLine();
+        if(num == "")break;
+        if(Convert.ToInt32(num)<=0)continue;
+        else count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Input number");
+Console.WriteLine(Counter());
