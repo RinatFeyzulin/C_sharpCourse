deb5525e500140921a390831183a5e787a366ebd
«Rinat» <rtatarin949@gmail.com>
2022-08-13 00:21:05 +0300

    7.1 Task Lesson

---
+++ 7_Lesson/7_1Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+
+void Print(int [,]arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j <columnSize; j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int [,] ArrayNum(int row, int column, int from, int to)
+{
+   int [,]arr = new int[row,column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <column; j++)
+        {
+            arr[i,j] = new Random().Next(from,to); 
+        }
+    }
+    return arr;
+}
+
+int [,] myArray = ArrayNum(3,5,1,100);
+Print(myArray);
