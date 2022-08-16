4b498673ba366d892c59b5851015fd2ced7a3d61
«Rinat» <rtatarin949@gmail.com>
2022-08-16 20:38:57 +0300

    task lesson 8_1

---
+++ 8_Lesson/8_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+void Print(int [,]arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j <columnSize; j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int [,] ArrayRandom(int row, int column)
+{
+   int [,]arr = new int[row,column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <column; j++)
+        {
+            arr[i,j] = new Random().Next(1,10); 
+        }
+    }
+    return arr;
+}
+
+int [,] Average(int [,] arr)
+{
+  
+
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < column; i++)
+    {        
+        (arr[0, i], arr[row - 1, i]) = (arr[row - 1, i], arr[0, i]);        
+    }
+    return arr;
+}
+
+int [,] myArray = ArrayRandom(4,4);
+Print(myArray);
+
+Console.WriteLine();
+
+Print(Average(myArray));
+
